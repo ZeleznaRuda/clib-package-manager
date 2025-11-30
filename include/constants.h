caf79fc6e6b9db13ed8778a4870ec5e7d98df47e
ZeleznaRuda <rudolfmullercz@gmail.com>
2025-11-30 14:14:19 +0100

    more secure installer

--- include/constants.h
+++ include/constants.h
@@ -10,6 +10,6 @@ inline fs::path getUserHomeDirectory() {
     return home ? fs::path(home) : fs::current_path();
 }
 
-inline std::string version = "1.1.4";
+inline std::string version = "1.1.8";
 inline fs::path homeDirectory = getUserHomeDirectory() / ".clibx";
 inline fs::path userHomeDirectory = getUserHomeDirectory();
--- sources/installer.cxx
+++ sources/installer.cxx
@@ -4,6 +4,7 @@
 #include <filesystem>
 #include <fstream>
 #include <unordered_map>
+#include <unistd.h>
 namespace fs = std::filesystem;
 std::unordered_map<std::string, std::string> data = {
     {"CMakeLists.txt", R"(
@@ -48,6 +49,10 @@ https://github.com/ZeleznaRuda/argvparser-engine.git
 )"}
 };
 int main(){
+    if (geteuid() == 0) {
+        std::cerr << "\033[31;1;3minstall error\033[0m: don't run the installer as administrator (root)." << std::endl;
+        return 1;
+    }
     try {
         const char* home = getenv("HOME");
         fs::path homeDirectory = fs::path(home) / ".clibx";
@@ -58,34 +63,55 @@ int main(){
             fs::create_directories(outputDirectory);
         }
         fs::copy_file(binPath, outputDirectory / "clibx", fs::copy_options::overwrite_existing);
-        std::fstream _bashrc (home + std::string("/.bashrc"), std::ios::in | std::ios::out | std::ios::app);
-        if (!_bashrc.is_open()){ 
+
+        std::ifstream bashIn(home + std::string("/.bashrc"));
+        std::string bashContent;
+        if (bashIn.is_open()) {
+            bashContent.assign((std::istreambuf_iterator<char>(bashIn)),
+                               std::istreambuf_iterator<char>());
+            bashIn.close();
+        } else {
             std::cerr << "\033[31;1;3minstall error\033[0m: cannot open .bashrc file" << std::endl;
             return 1;
         }
-        _bashrc.seekg(0, std::ios::beg);
-        std::string _bashrcContent((std::istreambuf_iterator<char>(_bashrc)), std::istreambuf_iterator<char>());
-        if (_bashrcContent.find("# clibx initialization") == std::string::npos) {
-            _bashrc.clear();
-            _bashrc << "\n# clibx initialization\n# if you don't know what you're doing, don't change the content !\n";
-            _bashrc << "export CLIBX_GIT_PATH=\"git\"\nexport CLIBX_HOME=\"" << homeDirectory.string() << "\"\n";
+
+        if (bashContent.find("# clibx initialization") == std::string::npos) {
+            std::ofstream bashOut(home + std::string("/.bashrc"), std::ios::app);
+            if (!bashOut.is_open()) {
+                std::cerr << "\033[31;1;3minstall error\033[0m: cannot open .bashrc file for writing" << std::endl;
+                return 1;
+            }
+            bashOut << "\n# clibx initialization\n# If you don't know what these environmental factors affect, don't change anything!\n";
+            bashOut << "export CLIBX_GIT_PATH=\"git\"\nexport CLIBX_HOME=\"" << homeDirectory.string() << "\"\n";
+            bashOut.close();
         }
 
-        fs::permissions((outputDirectory / "clibx"), fs::perms::owner_all | fs::perms::group_read | fs::perms::group_exec | fs::perms::others_read | fs::perms::others_exec, fs::perm_options::add);
-        if (fs::create_directories(homeDirectory / "_sys") && fs::create_directories(homeDirectory / "_sys" / "templates") && fs::create_directories(homeDirectory / "_sys" / "libRecords")  ) {
+        fs::permissions((outputDirectory / "clibx"),
+                        fs::perms::owner_all |
+                        fs::perms::group_read | fs::perms::group_exec |
+                        fs::perms::others_read | fs::perms::others_exec,
+                        fs::perm_options::add);
+
+        if (fs::create_directories(homeDirectory / "_sys") &&
+            fs::create_directories(homeDirectory / "_sys" / "templates") &&
+            fs::create_directories(homeDirectory / "_sys" / "libRecords")) {
+
             std::ofstream READMEFile(homeDirectory / "README.md");
             READMEFile << "DO NOT MODIFY THIS FOLDER" << std::endl;
             READMEFile.close();
-        for (const auto& [filename, content] : data) {
-            std::ofstream file(homeDirectory / "_sys" / "templates" / filename);
-            if (!file.is_open()) std::cout << "\033[31;1;3mcannot open file\033[0m" << std::endl;
-            file << content;
-            file.close();
+
+            for (const auto& [filename, content] : data) {
+                std::ofstream file(homeDirectory / "_sys" / "templates" / filename);
+                if (!file.is_open()) std::cout << "\033[31;1;3mcannot open file\033[0m" << std::endl;
+                file << content;
+                file.close();
+            }
         }
-        } else {}
+
         std::cout << "\033[32;1minstallation completed (after restarting the device or executing the 'rc' files: clibx will be fully functional ;) )\033[0m" << std::endl;
     } catch (const std::exception& e) {
         std::cerr << "\033[31;1;3minstall error\033[0m: " << e.what() << std::endl;
     }
+
     return 0;
 }
